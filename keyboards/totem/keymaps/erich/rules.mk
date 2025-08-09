73128462cc81d15f0160e26a239bafe4b2d6bc80
derDicke <info@300watt.de>
2025-08-09 20:10:59 +0200

    ftghfgh

--- keyboards/totem/keymaps/erich/rules.mk
+++ keyboards/totem/keymaps/erich/rules.mk
@@ -4,3 +4,8 @@ DYNAMIC_MACRO_ENABLE = yes
 KEY_OVERRIDE_ENABLE = no
 DYNAMIC_KEYMAP_LAYER_COUNT = 5
 SPLIT_KEYBOARD = yes
+# Enable debug information in the final binaries
+DEBUG_ENABLE = yes
+# Disable optimisations for debugging purposes
+LTO_ENABLE = no
+OPT = g
